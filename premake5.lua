858a3f7852263eeb1c63cbe98e730b586c99df36
Nate Rupsis <C-nathaniel.rupsis@charter.com>
2023-03-27 11:29:07 -0400

    renaming template stuff

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "Vulkano"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "Vulkano"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "Vulkano"
